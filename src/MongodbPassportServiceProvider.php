9dc66323e552e41d4dcd4a19531ab471028cc33e
saibotk <git@saibotk.de>
2020-02-23 00:18:03 +0100

    Remove unnecessary repositories and use Passport methods for overwrite

--- src/MongodbPassportServiceProvider.php
+++ src/MongodbPassportServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace DesignMyNight\Mongodb;
 
-use DesignMyNight\Mongodb\Passport\Bridge\RefreshTokenRepository as BridgeRefreshTokenRepository;
-use DesignMyNight\Mongodb\Passport\RefreshTokenRepository;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+
 use DesignMyNight\Mongodb\Passport\AuthCode;
 use DesignMyNight\Mongodb\Passport\Client;
 use DesignMyNight\Mongodb\Passport\PersonalAccessClient;
@@ -12,53 +12,16 @@ use DesignMyNight\Mongodb\Passport\Token;
 
 class MongodbPassportServiceProvider extends ServiceProvider
 {
-    /** @var string */
-    private $method = 'alias';
-
-    /** @var array */
-    private $aliases = [
-        'Laravel\Passport\AuthCode' => AuthCode::class,
-        'Laravel\Passport\Bridge\RefreshTokenRepository' => BridgeRefreshTokenRepository::class,
-        'Laravel\Passport\Client' => Client::class,
-        'Laravel\Passport\PersonalAccessClient' => PersonalAccessClient::class,
-        'Laravel\Passport\RefreshTokenRepository' => RefreshTokenRepository::class,
-        'Laravel\Passport\Token' => Token::class,
-    ];
-
-    /** @var Illuminate\Foundation\AliasLoader */
-    private $loader;
 
     /**
      * @return void
      */
     public function register()
     {
-        if (class_exists($loader = 'Illuminate\Foundation\AliasLoader')) {
-            $this->loader = $loader::getInstance();
-            $this->method = 'loadAlias';
-        }
+        Passport::useTokenModel(Token::class);
+        Passport::useClientModel(Client::class);
+        Passport::useAuthCodeModel(AuthCode::class);
+        Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
 
-        foreach ($this->aliases as $original => $alias) {
-            $this->{$this->method}($original, $alias);
-        }
-    }
-
-    /**
-     * @param string $original
-     * @param string $alias
-     */
-    private function alias(string $original, string $alias): void
-    {
-        class_alias($original, $alias);
-    }
-
-    /**
-     * @param string $original
-     * @param string $alias
-     */
-    private function loadAlias(string $original, string $alias): void
-    {
-        // Passport client extends Eloquent model by default, so we alias them.
-        $this->loader->alias($original, $alias);
     }
 }
