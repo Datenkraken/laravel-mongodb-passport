3401f38b31f582aca7557dda65938cf6bd42ddab
saibotk <git@saibotk.de>
2020-02-23 01:10:12 +0100

    Add missing Passport import

--- src/Passport/PersonalAccessClient.php
+++ src/Passport/PersonalAccessClient.php
@@ -3,6 +3,7 @@
 namespace DesignMyNight\Mongodb\Passport;
 
 use Jenssegers\Mongodb\Eloquent\Model;
+use Laravel\Passport\Passport;
 
 /**
  * This is the same as the original class in https://github.com/laravel/passport, except for
